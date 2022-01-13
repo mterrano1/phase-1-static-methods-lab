23c6d954180989f39c3b0063f129f8e9fe7a6345
Christopher Terrano <mterrano1@yahoo.com>
2022-01-13 02:13:09 -0600

    commit tree 4adfa3933188734177b5cd1c8a014e5f4e21853d

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'-\s]+/g, '');
+  }
 }
